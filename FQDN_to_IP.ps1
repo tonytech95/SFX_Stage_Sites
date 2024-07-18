13f4538e22241ed63de5da3bd7a53092f46eba13
tonytech95 <tony.yu@techordia.com>
2024-07-18 15:24:37 -0700

    Change to write file in repo

--- FQDN_to_IP.ps1
+++ FQDN_to_IP.ps1
@@ -1,8 +1,21 @@
-# Read FQDN from the array
+param (
+    $hostnameFilePath = ".\hostnames.txt",
+    $ipAddressFilePath = ".\ipaddresses.txt"
+)
+
+# Check to see if the ipaddresses.txt file exists; if not, create one with a carriage return.
+if (!(Test-Path $ipAddressFilePath)) {
+    New-Item -ItemType File -Path $ipAddressFilePath
+    "`r" | Out-File -FilePath $ipAddressFilePath -Encoding ASCII -Append
+}
+
+# Read hostnames from the input file
 $hostnames = @('exn-api3-stage.sfxresorts.net','exn-api3.sfxresorts.net','exn3-stage.sfxresorts.com','www-stage.vacationclix.com','www-stage.sfxresorts.com','members-stage.vacationclix.com','members3-stage.sfxresorts.com','exn3.sfxresorts.com','exn3-dev.sfxresorts.com')
 
 # Initialize an array to store IP addresses
 $ipAddresses = @()
+
+Write-Host $hostnames
 
 # Loop through each hostname, resolve its IP address, and add it to the array
 foreach ($hostname in $hostnames) {
@@ -17,22 +30,25 @@ foreach ($hostname in $hostnames) {
 # Sort and deduplicate the IP addresses
 $ipAddresses = $ipAddresses | Sort-Object | Get-Unique
 
+Write-Host $ipAddresses
 
 # Convert the IP addresses to a single string separated by newline
 $ipAddressesString = $ipAddresses -join "`r`n"
 
+# Save the sorted IP addresses to the temporary output text file
+$ipAddressesString | Out-File -FilePath .\ipaddresses-tmp.txt -Encoding Ascii -Force
 
-# Get current Time
-$checktime = Get-Date
+# Compare the current and previous IP addresses
+$areDifferences = @(Compare-Object (Get-Content -Path $ipAddressFilePath) (Get-Content -Path .\ipaddresses-tmp.txt))
 
-# Update Github Gist
-$headers = @{
-    "Accept" = "application/vnd.github+json"
-    "Authorization" = "Bearer $env:github_key"
-    "X-GitHub-Api-Version" = "2022-11-28"
+if ($areDifferences) {
+    # Differences found. Update the main output text file.
+    Move-Item -Path .\ipaddresses-tmp.txt -Destination $ipAddressFilePath -Force
+    Write-Host "Differences found. IP addresses updated."
+	
 }
-Invoke-RestMethod -Uri "https://api.github.com/gists/b8915f9d80a866f6c53c6323e978ab7e" `
-    -Method Patch `
-    -Headers $headers `
-    -ContentType "application/x-www-form-urlencoded" `
-    -Body "{`"description`":`"Last Check time: $checktime`",`"files`":{`"SFX_Tester.txt`":{`"content`":`"$ipAddressesString`"}}}"
+else {
+    # No differences found. Clean up the temporary file.
+    # Remove-Item -Path .\ipaddresses-tmp.txt -Force
+    Write-Host "No differences found. IP addresses unchanged."
+}
